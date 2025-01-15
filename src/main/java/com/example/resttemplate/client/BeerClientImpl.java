bd5bbebe1e1029daccae654c4811144e2bb3fc70
Rob Blake <rob.bl8ke@outlook.com>
2025-01-15 15:06:56 +0200

    GET parse using Jackson and interrogate JSON content (parse out beer names)

--- src/main/java/com/example/resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/example/resttemplate/client/BeerClientImpl.java
@@ -1,6 +1,7 @@
 package com.example.resttemplate.client;
 
 import com.example.resttemplate.model.BeerDTO;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
@@ -22,9 +23,14 @@ public class BeerClientImpl implements BeerClient {
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        // Use a map to get data back in a semi-structured form. Good approach if not sure what is being returned by the API
-        // and would simply want to interrogate the information coming back.
-        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+//        // Use a map to get data back in a semi-structured form. Good approach if not sure what is being returned by the API
+//        // and would simply want to interrogate the information coming back.
+//        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+
+        ResponseEntity<JsonNode> jsonResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
+        jsonResponse.getBody().findPath("content").elements().forEachRemaining(node -> {
+            System.out.println(node.get("beerName").asText());
+        });
 
         ResponseEntity<String> stringResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
 
