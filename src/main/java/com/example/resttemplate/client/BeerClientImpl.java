ea3aafb7dbfb72ba13799467f8f576e8b2acced8
Rob Blake <rob.bl8ke@outlook.com>
2025-01-15 15:00:34 +0200

    First successful client GET request (retrieve response as a map)

--- src/main/java/com/example/resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/example/resttemplate/client/BeerClientImpl.java
@@ -8,17 +8,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String> stringResponse = restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+        // Use a map to get data back in a semi-structured form. Good approach if not sure what is being returned by the API
+        // and would simply want to interrogate the information coming back.
+        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+
+        ResponseEntity<String> stringResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
 
         System.out.println((stringResponse.getBody()));
 
