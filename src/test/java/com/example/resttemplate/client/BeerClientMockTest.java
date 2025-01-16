41b2b57d726c44748bc4a2ba58f67d5fdb47f61b
Rob Blake <rob.bl8ke@outlook.com>
2025-01-16 15:44:28 +0200

    Testing RestTemplate: Successful attempt to test using MockRestServiceServer

--- src/test/java/com/example/resttemplate/client/BeerClientMockTest.java
+++ src/test/java/com/example/resttemplate/client/BeerClientMockTest.java
@@ -1,54 +1,74 @@
 package com.example.resttemplate.client;
 
+import com.example.resttemplate.config.RestTemplateBuilderConfig;
 import com.example.resttemplate.model.BeerDTO;
 import com.example.resttemplate.model.BeerDTOPageImpl;
 import com.example.resttemplate.model.BeerStyle;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.web.client.MockServerRestTemplateCustomizer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.UUID;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-@RestClientTest(BeerClientImpl.class)
+@RestClientTest
+@Import(RestTemplateBuilderConfig.class)
 public class BeerClientMockTest {
 
     static final String URL = "http://localhost:8080";
 
-    @Autowired
     BeerClient beerClient;
-
-    @Autowired
     MockRestServiceServer server;
 
     @Autowired
+    RestTemplateBuilder restTemplateBuilderConfigured;
+
+    @Mock
+    RestTemplateBuilder mockRestTemplateBuilder = new RestTemplateBuilder(new MockServerRestTemplateCustomizer());
+
+    @Autowired
     ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        RestTemplate restTemplate = restTemplateBuilderConfigured.build();
+        server = MockRestServiceServer.bindTo(restTemplate).build();
+
+        // Mockito behavior-driven development
+        when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);
+
+        beerClient = new BeerClientImpl(mockRestTemplateBuilder);
+    }
 
     @Test
     void testListBeers() throws JsonProcessingException {
         String payload = objectMapper.writeValueAsString(getPage());
 
-        // Watch out for the error: "Unable to use auto-configured MockRestServiceServer since a mock server customizer
-        // has not been bound to a RestTemplate or RestClient". Problem is that the RestTemplateBuilder.
-        // There may be a solution around this but for now need to use the video work-around.
-        // https://www.udemy.com/course/spring-framework-6-beginner-to-guru/learn/lecture/35497842#notes
-        // demonstrated in the next commit...
+        // Expect the request and response to behave like so...
         server.expect(method(HttpMethod.GET))
                 .andExpect(requestTo(URL + BeerClientImpl.GET_BEER_PATH))
                 .andRespond(withSuccess(payload, MediaType.APPLICATION_JSON));
 
+        // Expect that content will be returned.
         Page<BeerDTO> dtos = beerClient.listBeers();
         assertThat(dtos.getContent().size()).isGreaterThan(0);
     }
